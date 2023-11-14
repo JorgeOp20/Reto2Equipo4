58b5a896ccbed546258bdf3380d154e87a511cb3
JorgeOp20 <jorge.ortiz.perez@dxc.com>
2023-11-14 17:59:42 +0100

    Parametrizar test caso OK

--- src/test/java/es/netmind/mypersonalbankapi/persistencia/ClientesInDBRepoTest.java
+++ src/test/java/es/netmind/mypersonalbankapi/persistencia/ClientesInDBRepoTest.java
@@ -5,10 +5,13 @@ import es.netmind.mypersonalbankapi.modelos.clientes.Empresa;
 import es.netmind.mypersonalbankapi.modelos.clientes.Personal;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,26 +25,22 @@ class ClientesInDBRepoTest {
 
     @BeforeEach
     void setUp() throws Exception {
-        //repo = new ClientesInMemoryRepo();
         repo = new ClientesInDBRepo();
     }
 
-    @Test
-    void dadoAltaNuevoClienteTipoPersonalCuandoClienteValidoEntoncesOK() throws Exception {
 
-        Cliente altaUsuario = new Personal(null, "Equipo 4", "e4@gmai.com", "Calle 4", LocalDate.now(), true, false, "12345678Z" );
-        repo.addClient(altaUsuario);
+    @ParameterizedTest
+    @CsvSource({"Personal,Equipo4_Nuevo,e4@gmail.com,Calle cuatro,12345678Z", "Empresa,Empresa4 SA,emp4@gmail.com,Calle 44,A12345678"})
+    void dadoAltaNuevoClienteCuandoClienteValidoEntoncesOK(String tipoCliente, String nombre, String mail, String direccion, String idDocumento) throws Exception {
 
-        System.out.println(altaUsuario);
+        Cliente altaUsuario = null;
 
-        assertThat(altaUsuario.getId(), greaterThan(0));
+        if (Objects.equals(tipoCliente, "Personal")) {
+            altaUsuario = new Personal(null, nombre, mail, direccion, LocalDate.now(), true, false, idDocumento);
+        } else {
+            altaUsuario = new Empresa(null, nombre, mail, direccion, LocalDate.now(), true, false, idDocumento, null );
+        }
 
-    }
-
-    @Test
-    void dadoAltaNuevoClienteTipoEmpresaCuandoClienteValidoEntoncesOK() throws Exception {
-
-        Cliente altaUsuario = new Empresa(null, "Empresa 44", "e4@gmai.com", "Calle 4", LocalDate.now(), true, false, "Z12345678", null );
         repo.addClient(altaUsuario);
 
         System.out.println(altaUsuario);
@@ -56,7 +55,6 @@ class ClientesInDBRepoTest {
         Cliente altaUsuario = null;
 
         assertThrows(Exception.class, ()->{
-            //Cuando
             repo.addClient(altaUsuario);
         });
     }
